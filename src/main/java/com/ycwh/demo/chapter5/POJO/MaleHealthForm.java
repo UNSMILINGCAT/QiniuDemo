09feebfd1757f0585a8f0105dc0b640592c6c7c4
不笑猫 <867974171@qq.com>
2018-09-08 17:55:48 +0800

    练习

---
+++ src/main/java/com/ycwh/demo/chapter5/POJO/MaleHealthForm.java
@@ -0,0 +1,19 @@
+package com.ycwh.demo.chapter5.POJO;
+
+/**
+ * 男性体检表
+ */
+public class MaleHealthForm extends HealthForm
+{
+    private String prostate;
+
+    public String getProstate()
+    {
+        return prostate;
+    }
+
+    public void setProstate(String prostate)
+    {
+        this.prostate = prostate;
+    }
+}
