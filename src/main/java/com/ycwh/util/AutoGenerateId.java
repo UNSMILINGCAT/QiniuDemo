fbf8a0ad8f05991f890691379e203272937cc8cd
不笑猫 <867974171@qq.com>
2018-09-10 23:10:26 +0800

    练习

--- src/main/java/com/ycwh/util/AutoGenerateId.java
+++ src/main/java/com/ycwh/util/AutoGenerateId.java
@@ -5,11 +5,11 @@ import java.util.Date;
 
 public class AutoGenerateId
 {
-    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yymmdd");
+    public static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yymmddhhmm");
 
     public static int getAutoGenerateId()
     {
-
+        return Integer.parseInt(simpleDateFormat.format(new Date()));
     }
 
 }
