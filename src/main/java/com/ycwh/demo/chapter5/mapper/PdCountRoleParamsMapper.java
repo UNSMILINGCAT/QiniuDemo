cbc9adf2f2315efef4bfe125f5a70716919addbc
不笑猫 <867974171@qq.com>
2018-09-13 18:05:00 +0800

    练习

---
+++ src/main/java/com/ycwh/demo/chapter5/mapper/PdCountRoleParamsMapper.java
@@ -0,0 +1,8 @@
+package com.ycwh.demo.chapter5.mapper;
+
+import com.ycwh.demo.chapter5.POJO.PdCountRoleParams;
+
+public interface PdCountRoleParamsMapper
+{
+    void countRole(PdCountRoleParams pdCountRoleParams);
+}
